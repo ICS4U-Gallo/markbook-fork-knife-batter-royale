8e93412bb9a96f777fac1497f0b56f5414b75054
ejeng888 <ejeng888@gmail.com>
2019-09-26 09:59:02 -0400

    b

--- markbook.py
+++ markbook.py
@@ -3,6 +3,7 @@ Markbook Application
 Group members: Aidan, Ryan, Henson, Eric 
 """
 import pickle
+from typing import dict
 
 all_courses = []
 all_students = []
@@ -51,6 +52,7 @@ class course:
 
     def class_average(self):
         pass
+
 
 class student:
     def __init__(self, first_name, last_name, stu_num):
@@ -252,60 +254,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-from typing import Dict
-
-
-def create_assignment(name: str, due: str, points: int) -> Dict:
-    """Creates an assignment represented as a dictionary
-    
-    Args:
-        name: the name of the assignment.
-        due: the due date for the assignment.
-        points: what the assignment is out of (denominator).
-    Returns:
-        Assignment as a dictionary.
-    """
-    return {}
-
-
-def create_classroom(course_code: str, course_name: str, period: int, teacher: str) -> Dict:
-    """Creates a classroom dictionary"""
-    return {}
-
-
-def calculate_average_mark(student: Dict) -> float:
-    """Calculates the average mark of a student"""
-    return 0
-
-
-def add_student_to_classroom(student, classroom):
-    """Adds student to a classroom
-
-    Args:
-        student: Student dict
-        classroom: The classroom to add the student to
-    """
-    pass
-
-
-def remove_student_from_classroom(student: Dict, classroom: Dict):
-    """Removes student from classroom
-
-    Args:
-        student: The student to be removed
-        classroom: the class from which the student will be removed.
-    """
-    pass
-
-
-def edit_student(student: Dict, **kwargs: Dict):
-    """Edits the student's info with the provided key/value pairs
-
-    Args:
-        student: The student whose data needs to be udated.
-        **kwargs: KeyWordARGumentS. The key/value pairs of the
-            data that needs to be changed. Can come in the form
-            of a dictionary.
-    """
-    pass
